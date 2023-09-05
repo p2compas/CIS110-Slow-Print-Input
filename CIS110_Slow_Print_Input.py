e15039fd9672bb68465b411ae8110b9c75d72a3e
p2compas <p2compas@gmail.com>
2023-09-05 16:05:13 -0400

    Defined slowPrint

--- CIS110_Slow_Print_Input.py
+++ CIS110_Slow_Print_Input.py
@@ -1,3 +1,21 @@
-# Define two functions
+# Define two functions:
 # One to print one character at a time, preferrably by using formatted strings as input
 # The other to display input prompts slowly and return the result from input('')
+
+# Function prototypes:
+# void slowPrint(f-string s)
+# str slowInput(f-string s)
+
+# Import dependencies
+from time import sleep
+
+def slowPrint(s):
+    delay = 0.1
+    string = s
+    for char in s:
+        print(char, end="")
+        sleep(delay)
+    print()
+
+catName = 'Garfield'
+slowPrint(f"Hello world, {catName} is the best cat!")
