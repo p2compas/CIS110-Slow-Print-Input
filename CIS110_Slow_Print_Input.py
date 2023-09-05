7b2abb937f72892f962c546bbac2f3ee201e35df
p2compas <p2compas@gmail.com>
2023-09-05 17:41:54 -0400

    Added delay parameter, type checking for new parameter

--- CIS110_Slow_Print_Input.py
+++ CIS110_Slow_Print_Input.py
@@ -3,20 +3,19 @@
 # The other to display input prompts slowly and return the result from input('')
 
 # Function prototypes:
-# void slowPrint(*objects: object, sep: str | None = ' ', end: str | None = '\n')
-# str slowInput(prompt: str | object)
+# void slowPrint(*objects: object, sep: str | None = ' ', end: str | None = '\n', delay: float = 0.05)
+# str slowInput(prompt: str | object, delay: float = 0.05)
 
 # Import dependencies
 from time import sleep
 
-# Time to wait between characters
-delay = 0.025
-
-def slowPrint(*objects, sep=' ', end='\n'):
+def slowPrint(*objects, sep=' ', end='\n', delay=0.05):
     if not type(sep) in [str, None]:
         raise TypeError(f'sep must be None or a string, not {type(sep).__name__}')
     if not type(end) in [str, None]:
         raise TypeError(f'end must be None or a string, not {type(end).__name__}')
+    if not type(delay) in [float, int]:
+        raise TypeError(f'delay must be float or int, not {type(delay).__name__}')
     s, e, first = sep, end, True
     for obj in objects:
         print(f"{s if not first else ''}", end='')
@@ -26,25 +25,27 @@ def slowPrint(*objects, sep=' ', end='\n'):
         first = False
     print(end=e)
 
-def slowInput(prompt):
+def slowInput(prompt, delay=0.05):
+    if not type(delay) in [float, int]:
+        raise TypeError(f'delay must be float or int, not {type(delay).__name__}')
     for char in str(prompt):
         print(char, end="")
         sleep(delay)
     return input()
 
-# print lists
-lst = ['one', 'two', 'three', 'four']
-slowPrint(lst)
+if __name__ == '__main__':
+    # print lists
+    lst = ['one', 'two', 'three', 'four']
+    slowPrint(lst)
 
-# print objects
-slowPrint(slowPrint)
-slowPrint(slowInput)
+    # print objects
+    slowPrint(slowPrint)
+    slowPrint(slowInput)
 
-# input
-name = slowInput("\nThis is a slow input prompt, What\'s your name?  ")
+    # input
+    name = slowInput("\nThis is a slow input prompt, What\'s your name?  ")
+    num = slowInput(f"Hello, {name}! What\'s your favorite number?  ")
 
-num = slowInput(f"Hello, {name}! What\'s your favorite number?  ")
-
-# output
-slowPrint(f"Your name is {name} and your favorite number is {num}.")
-slowPrint("Your name is", name, "and your favorite number is", num, end=".\n")
+    # output
+    slowPrint(f"Your name is {name} and your favorite number is {num}.")
+    slowPrint("Your name is", name, "and your favorite number is", num, end=".\n")
