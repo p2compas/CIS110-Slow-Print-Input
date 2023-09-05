935103649b5ab619230bc4897eb61be1ab8d3d4b
p2compas <p2compas@gmail.com>
2023-09-05 17:29:59 -0400

    Modified to accept arbitrary arguments as well as standard print arguments sep and end.  Raise TypeError is arguments are not correct type.

--- CIS110_Slow_Print_Input.py
+++ CIS110_Slow_Print_Input.py
@@ -1,25 +1,33 @@
 # Define two functions:
-# One to print one character at a time, preferrably by using formatted strings as input
+# One to print one character at a time, using arbitrary arguments like the builtin print
 # The other to display input prompts slowly and return the result from input('')
 
 # Function prototypes:
-# void slowPrint(f-string s)
-# str slowInput(f-string s)
+# void slowPrint(*objects: object, sep: str | None = ' ', end: str | None = '\n')
+# str slowInput(prompt: str | object)
 
 # Import dependencies
 from time import sleep
 
 # Time to wait between characters
-delay = 0.05
+delay = 0.025
 
-def slowPrint(s):
-    for char in str(s):
-        print(char, end="")
-        sleep(delay)
-    print()
+def slowPrint(*objects, sep=' ', end='\n'):
+    if not type(sep) in [str, None]:
+        raise TypeError(f'sep must be None or a string, not {type(sep).__name__}')
+    if not type(end) in [str, None]:
+        raise TypeError(f'end must be None or a string, not {type(end).__name__}')
+    s, e, first = sep, end, True
+    for obj in objects:
+        print(f"{s if not first else ''}", end='')
+        for char in str(obj):
+            print(char, end='')
+            sleep(delay)
+        first = False
+    print(end=e)
 
-def slowInput(s):
-    for char in str(s):
+def slowInput(prompt):
+    for char in str(prompt):
         print(char, end="")
         sleep(delay)
     return input()
@@ -39,3 +47,4 @@ num = slowInput(f"Hello, {name}! What\'s your favorite number?  ")
 
 # output
 slowPrint(f"Your name is {name} and your favorite number is {num}.")
+slowPrint("Your name is", name, "and your favorite number is", num, end=".\n")
