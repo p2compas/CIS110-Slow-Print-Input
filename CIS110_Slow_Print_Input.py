4dad0ab2eb2d27ee3ca737c4a1afafb747e953df
p2compas <p2compas@gmail.com>
2023-09-05 15:50:36 -0400

    Project Defined

--- CIS110_Slow_Print_Input.py
+++ CIS110_Slow_Print_Input.py
@@ -1,1 +1,3 @@
-
+# Define two functions
+# One to print one character at a time, preferrably by using formatted strings as input
+# The other to display input prompts slowly and return the result from input('')
